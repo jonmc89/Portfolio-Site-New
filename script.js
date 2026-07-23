6f746e907591a0b16d02039f41be5b7f762b08c8
Jon <jonmckenziewebdev@gmail.com>
2026-07-23 12:53:16 +0100

    feat: :sparkles: sections
    added fade in animations for sections on scroll

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ menuBtn.addEventListener('click', () => {
   mobileNav.classList.toggle('visible');
   menuBtn.classList.toggle('highlighted');
 });
+
+// Section Fade
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-visbile');
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.1 },
+);
+
+document.querySelectorAll('.section-fade').forEach((section) => {
+  observer.observe(section);
+});
