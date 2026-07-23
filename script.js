a1ea4cb07d804d699146b0bc49886cd758e2e142
Jon <jonmckenziewebdev@gmail.com>
2026-07-23 18:48:11 +0100

    feat: :sparkles: contact form
    setup emailing service using emailjs

--- script.js
+++ script.js
@@ -25,3 +25,34 @@ const observer = new IntersectionObserver(
 document.querySelectorAll('.section-fade').forEach((section) => {
   observer.observe(section);
 });
+
+// Message Send
+
+emailjs.init({
+  publicKey: 'rNNP43bprqgFovXvD',
+});
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const submitButton = form.querySelector('.form-submit');
+  submitButton.disabled = true;
+  submitButton.value = 'Sending...';
+
+  try {
+    await emailjs.sendForm('service_a3lgzzt', 'template_hi7doq6', form);
+
+    submitButton.value = 'Message Sent!';
+    form.reset();
+  } catch (error) {
+    console.error('EmailJS error:', error);
+    submitButton.value = 'Failed to Send';
+  } finally {
+    setTimeout(() => {
+      submitButton.disabled = false;
+      submitButton.value = 'Send message';
+    }, 3000);
+  }
+});
